fd954b2a4793babb4d61556247330acd21355533
Philipp Schilter <philipp.schilter@screenconcept.ch>
2010-10-26 12:49:57 +0200

    added my_blog

---
+++ my_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = 'e70aedb6c100ee1f6015e7f88bbb75afc5a049884d28d646669212c96313eb4a9e63a18ec04d9a83a07ffe2e499ab02aa0f3b848c753402107df0f73dd1a3c50'
